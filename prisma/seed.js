6b85ef02d41e2d822fb00c3426a7e583bf062da0
Periklis <perik76@gmail.com>
2024-06-29 02:50:31 +0300

    create 3d user and 3d profile

--- prisma/seed.js
+++ prisma/seed.js
@@ -6,27 +6,34 @@ async function seed() {
 		data: [
 			{ username: "alice_m", email: "test1@test.db" },
 			{ username: "alice_m2", email: "test2@test.db" },
+			{ username: "John Doe", email: "jd@test.db" },
 		],
 	})
 
-    console.log(`${createdUsers.count} users created`, createdUsers)
-
+	console.log(`${createdUsers.count} users created`, createdUsers)
 
 	// Add your code here
 
 	const createProfiles = await prisma.profile.createManyAndReturn({
 		data: [
 			{
-                user_id: createdUsers[0].id,
+				user_id: createdUsers[0].id,
 				biography:
 					"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
 				picture_url: "Curabitur condimentum hendrerit tristique",
 			},
 			{
-                user_id: createdUsers[1].id,
+				user_id: createdUsers[1].id,
 				biography:
 					"Aliquam eleifend, mi id ullamcorper laoreet, tellus lorem dapibus erat, ac vehicula diam nulla a turpis",
 				picture_url: "Mauris tempus lectus ac magna mollis eleifend",
+			},
+			{
+				user_id: createdUsers[2].id,
+				biography:
+					"Lorem ipsum dolor sit amet, tellus lorem dapibus erat, ac vehicula diam nulla a turpis",
+				picture_url:
+					"Curabitur condimentum ac magna mollis eleifend",
 			},
 		],
 	})
